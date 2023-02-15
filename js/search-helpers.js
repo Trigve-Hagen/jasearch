60dffbd72c74e5e32737be91052fdb07c39c2a09
globalweb007 <globalwebmethods@gmail.com>
2023-02-15 07:31:57 -0500

    work on README.md file, commenting functions.

--- js/search-helpers.js
+++ js/search-helpers.js
@@ -1,6 +1,15 @@
 var SearchHelpers = (function () {
     var helper = {}
 
+    /**
+     * Public function
+     * Creates an id from a name.
+     * 
+     * Splits a name by spaces and replaces the spaces with dashes.
+     *
+     * @param string text
+     *   The text to create the id from.
+     */
     helper.createId = function (text) {
         var parts = text.toLowerCase().split(" ")
         var id = parts.join("-")
--- js/search-model.js
+++ js/search-model.js
@@ -1,6 +1,14 @@
 var SearchModel = (function () {
     var model = {}
 
+    /**
+     * Public function
+     * Gets JSON data via AJAX.
+     *
+     * @param string path
+     *   The path to retrieve the json either a function call in php script that returns
+     *   JSON or a JSON file.
+     */
     model.getData = function (path) {
         return (function () {
             var json = null;
